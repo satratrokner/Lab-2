19f2d410b15d1a32a5886e24fc0055b82fb3ba74
satratrokner <akatdar@gmail.com>
2024-11-05 08:47:22 +0300

    trtuyghghgtyy

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import ru.ifmo.se.pokemon.Battle;
+import ru.ifmo.se.pokemon.Pokemon;
+
+public class Main {
+    public static void main(String[] args) {
+        Battle b = new Battle();
+        Pokemon p1 = new Pokemon("Чужой", 1);
+        Pokemon p2 = new Pokemon("Хищник", 1);
+        b.addAlly(p1);
+        b.addFoe(p2);
+        b.go();
+    }
+}
